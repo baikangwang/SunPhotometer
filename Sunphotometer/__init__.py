14a153f1eaf8b57b91f43ef51edf984088bf6c2b
baikangwang <baikangwang@hotmail.com>
2017-11-20 01:39:58 +0800

    fix path

--- Sunphotometer/__init__.py
+++ Sunphotometer/__init__.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sppath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'SunPhotometer.source.jar')
+sppath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'SunPhotometer.jar')
 urarpath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'junrar-0.7.jar')
 sys.path.append(sppath)
 sys.path.append(urarpath)
--- download_aod.py
+++ download_aod.py
@@ -1,5 +1,6 @@
 import Sunphotometer.spdata as spdata
+import datetime
 
-ldir = 'd:/Working/Projects/SunPhotometer.source/data'
+ldir = 'd:/Working/Projects/SunPhotometer/data'
 t = datetime.datetime(2017,1,1)
 spdata.download(t, data_dir=ldir)
